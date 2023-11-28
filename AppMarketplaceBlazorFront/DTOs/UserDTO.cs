925ca896c40b111693d808d8776bca937d9924e0
Grigoriy Sergeev <39155149+GrigoriySergeev40103@users.noreply.github.com>
2023-11-28 23:22:14 +0300

    changes

---
+++ AppMarketplaceBlazorFront/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AppMarketplaceBlazorFront.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; set; } = default!;
+
+        public string? UserName { get; set; }
+    }
+}
