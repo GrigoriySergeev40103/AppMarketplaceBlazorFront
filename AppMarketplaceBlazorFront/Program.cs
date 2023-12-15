dcabe7f2916f661209dd1ce4da605e702c1b552e
Grigoriy Sergeev <39155149+GrigoriySergeev40103@users.noreply.github.com>
2023-12-15 14:20:20 +0300

    Refactoring

--- AppMarketplaceBlazorFront/Program.cs
+++ AppMarketplaceBlazorFront/Program.cs
@@ -2,7 +2,6 @@ using AppMarketplaceBlazorFront;
 using AppMarketplaceBlazorFront.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TusBlazorClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton<PersonalData>();
 builder.Services.AddScoped(sp => new HttpClient(new CookieHandler()) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddTusBlazorClient();
 
 await builder.Build().RunAsync();
